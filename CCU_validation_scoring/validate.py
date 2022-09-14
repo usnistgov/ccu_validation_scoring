42013aa744ed794cb57f90327c286e12863b069a
clyburn <clyburn.cunningham@nist.gov>
2022-09-14 13:15:06 -0400

    validate.py update
    Added my checks to validate.py
    Tested in validate_reference_draft.py
    Cleaned up unit tests

--- CCU_validation_scoring/validate.py
+++ CCU_validation_scoring/validate.py
@@ -1,3 +1,4 @@
+from functools import partial
 import os
 import pandas as pd
 from pathlib import Path
@@ -6,13 +7,60 @@ import logging
 
 logger = logging.getLogger('VALIDATION')
 
-def global_file_checks(task, reference_dir, submission_dir):
+def global_file_checks(task, reference_dir, submission_dir, scoring_index_file):
+    
+	try:
+		scoring_index = pd.read_csv(scoring_index_file, usecols = ['file_id'], sep = "\t")
+	except Exception as e:
+		logger.error('{} is not a valid scoring index file'.format(scoring_index_file))
+		exit(1)
 
-	ref = preprocess_reference_dir(ref_dir = reference_dir, task = task)
+	ref = preprocess_reference_dir(ref_dir = reference_dir, scoring_index = scoring_index, task = task)
 	index_file_path, subm_file_dict = check_index_get_submission_files(ref, submission_dir)
 	check_submission_files(submission_dir, index_file_path, subm_file_dict)
 
 	return ref, subm_file_dict
+
+def validate_ref_individial_file_check(task, ref_file_path, column_map, header_map, ref_file):
+	if task == "norms":
+		file_checks = (check_valid_tab(ref_file_path) and
+			check_column_number(ref_file_path,column_map[task]) and
+			check_valid_header(ref_file_path,list(header_map[task])) and
+			#check_output_records(subm_file_path, task, processed_label) and
+			#check_data_type(subm_file_path, header_map[task]) and 
+			check_fileid_index_match(ref_file_path, ref_file) and 
+			check_norm_range(ref_file_path) )
+	
+	if task == "emotions":
+		file_checks =(check_valid_tab(ref_file_path) and
+			check_column_number(ref_file_path,column_map[task]) and
+			check_valid_header(ref_file_path,list(header_map[task])) and
+			#check_output_records(subm_file_path, task, processed_label) and
+			#check_data_type(subm_file_path, header_map[task]) and 
+			check_fileid_index_match(ref_file_path, ref_file) and
+			check_emotion_id(ref_file_path) and 
+			check_duplicate_emotions(ref_file_path) and
+			check_empty_na(task, ref_file_path))
+
+	if task == "valence_continuous" or task == "arousal_continuous":
+		file_checks =(check_valid_tab(ref_file_path) and
+			check_column_number(ref_file_path,column_map[task]) and
+			check_valid_header(ref_file_path,list(header_map[task])) and
+			#check_output_records(subm_file_path, task, processed_label) and
+			#check_data_type(subm_file_path, header_map[task]) and 
+			check_fileid_index_match(ref_file_path, ref_file) and 
+			check_value_range(ref_file_path, task) and
+			check_nospeech_all_columns(ref_file_path) and 
+			check_nospeech_all_annotators(ref_file_path))	
+
+	if task == "segment":
+		file_checks =(check_valid_tab(ref_file_path) and
+			check_column_number(ref_file_path,column_map[task]) and
+			check_valid_header(ref_file_path,list(header_map[task])) and
+			#check_output_records(subm_file_path, task, processed_label) and
+			#check_data_type(subm_file_path, header_map[task]) and 
+			check_fileid_index_match(ref_file_path, ref_file) and 
+			check_start_small_end(ref_file_path))
 
 def individual_file_check(task, subm_file_path, column_map, header_map, processed_label, subm_file, length, ref_df, norm_list):
 	
@@ -362,7 +410,7 @@ def check_ref_norm(hidden_norm, mapping_file):
 	return True
 
 def extract_modality_info(file_type):
-
+		
 	if file_type == "text":
 		frame_data_type = "int"
 	else:
@@ -376,3 +424,156 @@ def extract_modality_info(file_type):
 				"changepoint":{"file_id": "object","timestamp": frame_data_type,"llr": "float"}}
 
 	return column_map, header_map
+	
+def check_duplicate_emotions(file):
+	
+	df = pd.read_csv(file, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+	for index, row in df.iterrows():
+		emotion_list = row['emotion'].split()
+		uniq_emotions = set(emotion_list)
+		if len(emotion_list) != len(uniq_emotions):
+			logger.error("Validation failed")
+			seen = set()
+			duplicates = [emotion for emotion in emotion_list if emotion in seen or seen.add(emotion)]
+			if duplicates:
+				logger.error("File {} contains the following emotion(s) duplicated: {}".format(file, duplicates))
+
+def check_empty_na(task, file):
+	
+	df = pd.read_csv(file, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+		
+	for index, row in df.iterrows():
+		# Check second to last row (row['emotion'] for emotions.tab and row['norm'])
+		if task == "emotions":
+			if row["emotion"] == "none" and row['multi_speaker'] != "EMPTY_NA":
+				logger.error("Validation failed {}".format(file))
+				logger.error("Expected 'EMPTY_NA' status, got {}".format(row['multi_speaker']))
+		if task == "norms":
+			if row["norm"] == "none" and row['status'] != "EMPTY_NA":
+				logger.error("Validation failed {}".format(file))
+				logger.error("Expected 'EMPTY_NA' status, got {}".format(row['status']))
+
+def check_nospeech_all_columns(file):
+		
+	df = pd.read_csv(file, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+	for index, row in df.iterrows():
+		val_and_arousal_tags = [row['valence_continuous'], row['valence_binned'], row['arousal_continuous'], row['arousal_binned']]
+		if "nospeech" in val_and_arousal_tags:
+			nospeech_count = val_and_arousal_tags.count("nospeech")
+			if nospeech_count != 4:
+				logger.error("Validation failed")
+				logger.error("Expected 4 'nospeech' tags but only got {} in file {}".format(nospeech_count, file))
+					
+def check_nospeech_all_annotators(file):
+    	
+	# Make sure "nospeech" tags applied to all value columns
+	check_nospeech_all_columns(file)
+	
+	df = pd.read_csv(file, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+	
+	last_segment_id = ""
+	for index, row in df.iterrows():
+		# Create a partial dataframe containing only rows w/ this specific segment_id
+		current_segment_id = row['segment_id']
+		if current_segment_id != last_segment_id:
+			partial_df = df[df['segment_id'] == current_segment_id]
+		else:
+			continue
+
+		# Check if this segment_id has any "nospeech values". If so, make sure that all annotators said no speech
+		nospeech_df = partial_df[partial_df['valence_continuous'] == "nospeech"]
+		if nospeech_df.shape[0] == 0:
+			last_segment_id = current_segment_id
+			continue
+		else: 
+			# Retrieve user_ids of those who did not tag nospeech
+			missing_nospeech_df = partial_df[partial_df['valence_continuous'] != "nospeech"]
+			if missing_nospeech_df.shape[0] != 0:
+				logger.error("Validation failed")
+				logger.error("Inconsistent 'nospeech' tags in segment {}".format(current_segment_id))
+				logger.error("The following annotators tagged 'nospeech': {}".format(set(nospeech_df['user_id'])))
+				logger.error("The following annotators did not tag 'nospeech': {}".format(set(missing_nospeech_df['user_id'])))
+
+		last_segment_id = current_segment_id
+
+def check_norm_range(file):
+	
+	df = pd.read_csv(file, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+	valid_norms = ['001', '002', '007', 'none', 'nospeech'] #subject to change	
+	if df.shape[0] != 0:
+		invalid_norms = []
+		for norm in df['norm']:
+			if norm not in valid_norms:
+				invalid_norms.append(norm)
+
+		if len(invalid_norms) > 0:
+			logger.error("Invalid file {}", file)
+			logger.error("Additional emotion(s) '{}' have been found in {} ".format(set(invalid_norms), file))
+			return False
+	
+	return True
+
+def check_start_end_types(file, ref_dir):
+
+	# Open system input file and record which files are text and which files are audio/video using two lists
+	input_file_path = os.path.join(ref_dir, "system_input.index.tab")
+	check_file_exist(input_file_path, input_file_path, ref_dir)
+	input_df = pd.read_csv(input_file_path, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+
+	text_ids = []
+	audvid_ids = []
+	for index, row in input_df.iterrows():
+		if row['type'] == "text":
+			text_ids.append(row['file_id'])
+		elif row['type'] == "audio" or row['type'] == "video":
+			audvid_ids.append(row['file_id'])
+		else:
+			logger.error("Invalid file")
+			logger.error("File {} has a type other than text, audio, or video for file_id {}".format(input_file_path, row['file_id']))
+	
+	# Open segments.tab and for every file_id, if it's in one list, make sure it has a data type of ___ and vice versa otherwise
+	segments_df = pd.read_csv(file, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+	for index, row in segments_df.iterrows():
+		if row['file_id'] in text_ids:
+			if row['start'].is_integer() == row['end'].is_integer() == False:
+				logger.error("Validation failed")
+				logger.error("In file {}, file id {} contains start/end time that are not ints".format(file, row['file_id']))
+		elif row['file_id'] in audvid_ids:
+			if isinstance(row['start'], float) == isinstance(row['end'], float) == False:
+				logger.error("Validation failed")
+				logger.error("In file {}, file id {} contains start/end time that are not floats".format(file, row['file_id']))
+		else:
+			logger.error("Validation failed")
+			logger.error("Type of file ID {} in file {} is not text, audio, or video".format(row['file_id'], file))
+
+def check_fileid_segmentid_match(file, ref_dir):
+	# Read in file ids and segment ids from segments.tab
+	segments_file_path = os.path.join(ref_dir, "segments.tab")
+	check_file_exist(segments_file_path, segments_file_path, ref_dir)
+	segments_df = pd.read_csv(segments_file_path, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+
+	expected_file_ids = []
+	expected_segment_ids = []
+	for index, row in segments_df.iterrows():
+		if row['file_id'] not in expected_file_ids:
+			expected_file_ids.append(row['file_id'])
+		if row['segment_id'] not in expected_segment_ids:
+			expected_segment_ids.append(row['segment_id'])
+	
+	# For each of the other 3 files, loop through and make sure that the file and segment ids are in those lists
+	invalid_file_ids = []
+	invalid_segment_ids = []
+	df = pd.read_csv(file, dtype={'norm': object, 'sys_norm': object, 'ref_norm': object, 'message': object}, sep='\t')
+	if df.shape[0] != 0:
+		for index, row in df.iterrows():
+			if row['file_id'] not in expected_file_ids:
+				invalid_file_ids.append(row['file_id'])
+			if row['segment_id'] not in expected_segment_ids:
+				invalid_segment_ids.append(row['segment_id'])
+	
+		if len(invalid_file_ids) > 0 or len(invalid_segment_ids) > 0:
+			logger.error("Invalid file {}".format(file))
+			if len(invalid_file_ids) > 0:
+				logger.error("File ID(s) {} in {} is not found in {}".format(invalid_file_ids, file, segments_file_path))
+			if len(invalid_segment_ids) > 0:
+				logger.error("Segment ID(s) {} in {} is not found in {}".format(invalid_segment_ids, file, segments_file_path))
