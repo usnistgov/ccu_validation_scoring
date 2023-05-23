8622472f21089235025d29404a204ac8d6026c6a
Jonathan Fiscus <jonathan.fiscus@nist.gov>
2023-05-23 10:58:17 +0000

    Fixed a case with fhyp becomes empty

--- CCU_validation_scoring/score_norm_emotion.py
+++ CCU_validation_scoring/score_norm_emotion.py
@@ -309,7 +309,7 @@ def compute_ious(row, ref, class_type, time_span_scale_collar, text_span_scale_c
                 rout[['start_hyp', 'end_hyp', 'llr']] = row.start, row.end, row.llr
                 #print(rout)
         else:
-            #print(f"Internal Error: unknown alignment hack {align_hacks}")
+            print(f"Internal Error: unknown alignment hack {align_hacks}")
             exit(-1)
                 
         rout = rout.rename(columns={'start':'start_ref', 'end':'end_ref'})
@@ -442,8 +442,8 @@ def compute_average_precision_tad(ref, hyp, Class, iou_thresholds, task, time_sp
     ihyp.sort_values(["llr"], ascending=False, inplace=True)        
     ihyp.reset_index(inplace=True, drop=True)
 
-    #print(f"INFO: {len(ihyp[(ihyp.hyp_isTruncated==True) & (ihyp.isNSCR == True)].index)} hyp records for Class={Class} type={Type} are hyp_isTruncated==TRUE")
-    #print(ihyp[(ihyp.hyp_isTruncated==True) & (ihyp.isNSCR == True)])
+    print(f"INFO: {len(ihyp[(ihyp.hyp_isTruncated==True) & (ihyp.isNSCR == True)].index)} hyp records for Class={Class} type={Type} are hyp_isTruncated==TRUE")
+    print(ihyp[(ihyp.hyp_isTruncated==True) & (ihyp.isNSCR == True)])
 
     ### This is NEW FOR 1.3.X
     ### Drop False Alarms that are hyp_isTruncated == TRUE
@@ -532,34 +532,35 @@ def compute_average_precision_tad(ref, hyp, Class, iou_thresholds, task, time_sp
         #print(fhyp)
 
         measures = generate_zero_scores_norm_emotion(None)
-        llr = np.array(fhyp["llr"])
-        rec = (np.array(fhyp["cum_tp"]) / npos)
-        prec = (np.array(fhyp["cum_tp"]) / (np.array(fhyp["cum_tp"]) + np.array(fhyp["cum_fp"])))
-        
-        ### OK, add some more metrics!
-        nsys = fhyp.cum_tp.iat[-1] + fhyp.cum_fp.iat[-1]
-        ### pct_tp can have residual values from being aliged to a NO_SCORE REGION, SO, filter the sum
-        sum_scaled_tp = ihyp[ihyp.tp == 1].pct_tp.sum()
-        ### pct_fp is complicated.  It's two parts: (1) if it's labeled as FP, default to 1, (2) Sum of the pct_fp for TPs
-        sum_scaled_fp = 1.0 * len(ihyp[ihyp.fp == 1].pct_fp) + ihyp[ihyp.tp == 1].pct_fp.sum()
-        scaled_recall =    sum_scaled_tp / npos
-        scaled_precision = sum_scaled_tp / (sum_scaled_tp + sum_scaled_fp)  
-        
-        measures['AP'] = ap_interp(prec, rec)
-        measures['prcurve:precision'] = prec
-        measures['prcurve:recall'] = rec
-        measures['prcurve:llr'] = llr
-        measures['precision_at_MinLLR'] = prec[-1]
-        measures['recall_at_MinLLR'] = rec[-1]
-        measures['f1_at_MinLLR'] = f1(prec[-1], rec[-1])
-        measures['llr_at_MinLLR'] = llr[-1]
-        measures['sum_tp_at_MinLLR'] = ihyp.tp.sum()
-        measures['sum_fp_at_MinLLR'] = ihyp.fp.sum()
-        measures['sum_scaled_tp_at_MinLLR'] = sum_scaled_tp
-        measures['sum_scaled_fp_at_MinLLR'] = sum_scaled_fp
-        measures['scaled_recall_at_MinLLR'] =  scaled_recall
-        measures['scaled_precision_at_MinLLR'] =  scaled_precision
-        measures['scaled_f1_at_MinLLR'] =  f1(scaled_precision, scaled_recall)
+        if (len(fhyp.index) > 0):
+            llr = np.array(fhyp["llr"])
+            rec = (np.array(fhyp["cum_tp"]) / npos)
+            prec = (np.array(fhyp["cum_tp"]) / (np.array(fhyp["cum_tp"]) + np.array(fhyp["cum_fp"])))
+            
+            ### OK, add some more metrics!
+            nsys = fhyp.cum_tp.iat[-1] + fhyp.cum_fp.iat[-1]
+            ### pct_tp can have residual values from being aliged to a NO_SCORE REGION, SO, filter the sum
+            sum_scaled_tp = ihyp[ihyp.tp == 1].pct_tp.sum()
+            ### pct_fp is complicated.  It's two parts: (1) if it's labeled as FP, default to 1, (2) Sum of the pct_fp for TPs
+            sum_scaled_fp = 1.0 * len(ihyp[ihyp.fp == 1].pct_fp) + ihyp[ihyp.tp == 1].pct_fp.sum()
+            scaled_recall =    sum_scaled_tp / npos
+            scaled_precision = sum_scaled_tp / (sum_scaled_tp + sum_scaled_fp)  
+            
+            measures['AP'] = ap_interp(prec, rec)
+            measures['prcurve:precision'] = prec
+            measures['prcurve:recall'] = rec
+            measures['prcurve:llr'] = llr
+            measures['precision_at_MinLLR'] = prec[-1]
+            measures['recall_at_MinLLR'] = rec[-1]
+            measures['f1_at_MinLLR'] = f1(prec[-1], rec[-1])
+            measures['llr_at_MinLLR'] = llr[-1]
+            measures['sum_tp_at_MinLLR'] = ihyp.tp.sum()
+            measures['sum_fp_at_MinLLR'] = ihyp.fp.sum()
+            measures['sum_scaled_tp_at_MinLLR'] = sum_scaled_tp
+            measures['sum_scaled_fp_at_MinLLR'] = sum_scaled_fp
+            measures['scaled_recall_at_MinLLR'] =  scaled_recall
+            measures['scaled_precision_at_MinLLR'] =  scaled_precision
+            measures['scaled_f1_at_MinLLR'] =  f1(scaled_precision, scaled_recall)
 
         # if (Class == '102'):
         #     ihyp.to_csv(f"/mnt/ccu-vol/ccu_results/p1_minieval/AlignmentAnalysis-v5-check/out/ihyp.{Type}.{Class}.txt", sep = "\t", index = None)
